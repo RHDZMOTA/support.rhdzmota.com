a611b995f150e5805c23f753b7290d1477582351
rhdzmota <contact@rhdzmota.com>
2024-02-29 21:46:02 -0600

    Genesis - update streamlit command with common params

--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@
     python -m pip install -e rhdzmota_support;
     cd rhdzmota_support/src/main/streamlit_entrypoint;
     python build_pages.py build_all;
-    streamlit run Support.py
+    streamlit run Support.py \
+        --server.port ${SUPPORT_SERVER_PORT} \
+        --server.headless true \
+        --theme.base dark
 )
