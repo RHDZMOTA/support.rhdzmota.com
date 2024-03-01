df0ffc84bc9c9f95a40db33f1a02d2d39df2595b
rhdzmota <contact@rhdzmota.com>
2024-03-01 00:23:29 -0600

    Genesis - Include favicon

--- rhdzmota_support/src/main/streamlit_entrypoint/Support.py
+++ rhdzmota_support/src/main/streamlit_entrypoint/Support.py
@@ -1,5 +1,7 @@
+import streamlit as st 
 from rhdzmota.support.frontend.views.main import view
 
 
 if __name__ == "__main__":
+    st.set_page_config(page_icon="./favicon.png", layout="wide")
     view()
