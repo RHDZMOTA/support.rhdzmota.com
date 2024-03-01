be34cd1af9701a046f7f5641f4172aee3410231c
rhdzmota <contact@rhdzmota.com>
2024-03-01 17:48:49 -0600

    Genesis - Sentry support (testing)

--- rhdzmota_support/src/main/streamlit_entrypoint/Support.py
+++ rhdzmota_support/src/main/streamlit_entrypoint/Support.py
@@ -1,7 +1,17 @@
+import os
 import streamlit as st 
+import sentry_sdk
+
 from rhdzmota.support.frontend.views.main import view
 
 
 if __name__ == "__main__":
+    if (sentry_dns := os.environ.get("SENTRY_SDK_DNS")):
+        sentry_sdk.init(
+            dsn=sentry_dns,
+            traces_sample_rate=1.0,  # 1.0 is equivalent to capturing 100% of transactions for performance monitoring
+            profiles_sample_rate=1.0, # 1.0 is equivalent to profiling 100% of sampled transactions 
+        )
+
     st.set_page_config(page_icon="./favicon.png", layout="wide")
     view()
